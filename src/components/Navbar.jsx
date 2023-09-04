ae42010513b60c92a9358ace77cfdde06c2935fc
marshallanique <marshallanique@gmail.com>
2023-09-04 14:35:32 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 const Navbar = () => {
   return (
-    <div>
-      <h3>navbar</h3>
+    <div className="bg-black font-Mulish text-white  ">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center h-24">
+          {/* store */}
+          <div>
+            <h3 className="text-3xl">E commerce</h3>
+          </div>
+          {/* icon */}
+          <div className="relative">
+            <div className="z-20">
+              <MdOutlineShoppingCartCheckout style={{ fontSize: "50px" }} />
+            </div>
+            <div className="absolute left-8 top-[-10px]">
+              <div className="flex justify-center items-center bg-[#A29DFF] rounded-full h-8 w-8  text-base text-white">
+                0
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Mulish': ['Mulish', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
